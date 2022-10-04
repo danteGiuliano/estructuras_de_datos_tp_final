9e60e6acf39a9b87e0e61296f20a95525e821f7d
dante <dante.giuliano@est.fi.uncoma.edu.ar>
2022-10-04 10:30:26 -0300

    fix e implementacion de pila con peso

--- Estructuras/Grafo/TestGrafo.java
+++ Estructuras/Grafo/TestGrafo.java
@@ -11,9 +11,10 @@ public class TestGrafo {
         Grafo grafo = ORM.get_aeropuertos();
         System.out.println(grafo.toString());
         //System.out.println(grafo.toString());
-        Lista l = grafo.camino_mas_rapido("NQN", "CBA");
-        mostrar(l);
-        
+        Lista l1 = grafo.camino_mas_corto("NQN", "ETC");
+        Lista l2 = grafo.camino_mas_rapido("NDM","ETC");
+        mostrar(l1);
+        mostrar(l2);
 
     }
     public static void mostrar(Lista l){
